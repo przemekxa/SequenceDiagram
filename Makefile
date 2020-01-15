3c506c6abc5c495fd1d646af43edbc5ef266d9db
Przemek Ambroży <przemek19986@gmail.com>
2020-01-15 16:02:00 +0100

    Prepare backends

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
-all: main
+all: submodules main
 
 submodules:
 	git submodule update --init --recursive
+	./prepareBackends.sh
 
 main:
-	g++ -std=c++14 -lncurses umlSequence/*.cpp umlSequence/backend-nice/*.cpp umlSequence/backend-nano/*.cpp -o umlSequenceApp
+	g++ -std=c++14 -lncurses umlSequence/*.cpp umlSequence/backend-nice/*.cpp umlSequence/backend-nano/*.cpp -o main
---
+++ prepareBackend.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# nice
+sed -i.bak '73d' umlSequence/backend-nice/nice-like.cpp
+sed -i.bak '74i\
+tool->redraw();\
+' umlSequence/backend-nice/nice-like.cpp
+
+sed -i.bak '89,90d' umlSequence/backend-nice/nice-like.cpp
+
+sed -i.bak '89i\
+string p(1, c);\
+tool->setEntry("KEY", p);\
+' umlSequence/backend-nice/nice-like.cpp
+
+# nano
+sed -i.bak '6i\
+#include <string>\
+' umlSequence/backend-nano/shared.hpp
+
