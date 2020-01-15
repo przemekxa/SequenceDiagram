a3dbab5bc2890748269c0de8d8b002b6cd6ec71a
Przemek Ambroży <przemek19986@gmail.com>
2020-01-15 16:27:22 +0100

    Name change pt 2

--- Makefile
+++ Makefile
@@ -1,8 +1,7 @@
-all: submodules main
+main:
+	g++ -std=c++14 -lncurses sequenceDiagram/*.cpp sequenceDiagram/backend-nice/*.cpp sequenceDiagram/backend-nano/*.cpp -o main
 
-submodules:
+init:
 	git submodule update --init --recursive
 	./prepareBackends.sh
 
-main:
-	g++ -std=c++14 -lncurses sequenceDiagram/*.cpp sequenceDiagram/backend-nice/*.cpp sequenceDiagram/backend-nano/*.cpp -o main
--- prepareBackends.sh
+++ prepareBackends.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # nice
-sed -i.bak '73d' umlSequence/backend-nice/nice-like.cpp
+sed -i.bak '73d' sequenceDiagram/backend-nice/nice-like.cpp
 sed -i.bak '74i\
 tool->redraw();\
-' umlSequence/backend-nice/nice-like.cpp
+' sequenceDiagram/backend-nice/nice-like.cpp
 
-#sed -i.bak '89,90d' umlSequence/backend-nice/nice-like.cpp
+#sed -i.bak '89,90d' sequenceDiagram/backend-nice/nice-like.cpp
 
 #sed -i.bak '89i\
 #string p(1, c);\
 #tool->setEntry("KEY", p);\
-#' umlSequence/backend-nice/nice-like.cpp
+#' sequenceDiagram/backend-nice/nice-like.cpp
 
 # nano
-rm umlSequence/backend-nano/main.cpp
-rm umlSequence/backend-nano/main
+rm sequenceDiagram/backend-nano/main.cpp
+rm sequenceDiagram/backend-nano/main
 sed -i.bak '6i\
 #include <string>\
-' umlSequence/backend-nano/shared.hpp
+' sequenceDiagram/backend-nano/shared.hpp
 
